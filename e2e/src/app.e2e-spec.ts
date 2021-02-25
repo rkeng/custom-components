9c49fcec726130d9b011a6683d4072d525b9fa16
Ryan Keng <ryan.keng@logicsolutions.com>
2021-02-25 08:59:40 +0800

    rename project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('custom-datepicker app is running!');
+    expect(page.getTitleText()).toEqual('custom-components app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'custom-datepicker'`, () => {
+  it(`should have as title 'custom-components'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('custom-datepicker');
+    expect(app.title).toEqual('custom-components');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('custom-datepicker app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('custom-components app is running!');
   });
 });
